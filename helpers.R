0ca793beb0fb55d750ed88a44af8cee43b1f2ae7
Marius Sandu <marius.sandu@linksoft.ro>
2021-01-05 18:37:22 +0200

    - Removed get_headers function - Removed df_to_js function + Added convert_datetime function + Added convert_dax_format function which updates format strings. Works only for datetimes, dates and times. Returns char. + Added DT_to_HTML function which uses a template and dataframe to build an HTML table. Returns char.

--- helpers.R
+++ helpers.R
@@ -53,31 +53,121 @@ get_sample <- function(table_name) {
   
   expected_headers <- columns[
     columns$TableID == table_id 
-    & columns$IsHidden == 'FALSE', ExplicitName]
+    & columns$IsHidden == 'FALSE', c('ExplicitName', 'Conversion')]
+  
+  expected_headers$Conversion <- as.character(expected_headers$Conversion)
+  
+  headers_to_format <- expected_headers[expected_headers$Conversion != 'NULL']
   
   dax_query <- paste0("EVALUATE SAMPLE (5, '",table_name,"', 1)")
   result <- con$Execute(
     dax_query
   )$GetRows() %>%
-    rowset_to_DT(headers = expected_headers)
+    rowset_to_DT(headers = expected_headers$ExplicitName)
+  
+  # TODO: fix this convert deconvert crap
+  result <- as.data.frame(result)
+  
+  for (header_to_format in headers_to_format$ExplicitName) {
+    
+    format_string <- as.character( headers_to_format[headers_to_format$ExplicitName == header_to_format, Conversion])
+    
+    for (i in 1:nrow(result)) {
       
+      value_to_convert <- result[i, header_to_format]
+      result[i, header_to_format] <- convert_datetime(value_to_convert, format = format_string)
+      
+    }
+    
+  }
+  
+  result <- as.data.table(result)
+
   return(result)
   
 }
 
-get_headers <- function(content) {
-  result <- data.frame(
-    'data' = colnames(content),
-    'title' = colnames(content)
+convert_excel_date <- function(excel_date) {
+  return(
+    as.POSIXlt((excel_date * 86400), origin='1899-12-30', tz='GMT')
   )
-  return(result)
 }
 
-df_to_js <- function(x, var_name = "data", ...){
-  
-  json_data <- jsonlite::toJSON(x, dataframe = 'rows')
-  
-  htmltools::tags$script(paste0("var ",var_name," = ", json_data, ";"))
-  
+convert_datetime <- function(excel_date, format = '') {
+  return(
+    format( convert_excel_date( as.numeric(excel_date) ),  format = format )
+  )
 }
 
+convert_dax_format <- function(string) {
+  # Absolutely disgusting
+  initial_string <- string
+  string <- gsub('\\\\', '', string)
+  string <- gsub(' AM/PM', '', string)
+  string <- gsub('dddd', '%A', string)
+  string <- gsub('mmmm', '%B', string)
+  string <- gsub('yyyy', '%Y', string)
+  string <- gsub('ddd', '%a', string)
+  string <- gsub('mmm', '%b', string)
+  string <- gsub('dd', '%d', string)
+  string <- gsub('mm', '%m', string)
+  string <- gsub('yy', '%y', string)
+  string <- gsub('d', '%d', string)
+  string <- gsub('hh', '%H', string)
+  string <- gsub('h', '%H', string)
+  string <- gsub('nn', '%M', string)
+  string <- gsub('ss', '%S', string)
+  string <- gsub('General Date', '%Y-%m-%d %H:%M:%S', string)
+  
+  if (string == initial_string) {
+    return(NULL)
+  } else {
+    return(string)
+  }
+}
+
+DT_to_HTML <- function(DT, table_id, display) {
+  
+  headers <- colnames(DT)
+  
+  template <- readr::read_file('sample_tables_template/template.htm')
+  th <- strsplit(
+    strsplit(
+      strsplit(template, "}}")[[1]][1], "\\{\\{")[[1]][2], "@")[[1]]
+  tr <- strsplit(
+    strsplit(template, "]]")[[1]][1], "\\[\\[")[[1]][2]
+  td <- strsplit(
+    strsplit(
+      strsplit(template, "))")[[1]][1], "\\(\\(")[[1]][2], "@")[[1]]
+  
+  th_filled <- paste(th[1], headers, th[2], collapse = '')
+  
+  tr_filled <- ''
+  
+  for (i in 1:nrow(DT)) {
+    tds_filled <- paste(td[1], DT[i], td[2], collapse = '')
+    local_tr_filled <- gsub(
+      paste0("\\(\\(", paste0(td, collapse = "@") , "))"), 
+      tds_filled, 
+      tr)
+    tr_filled <- paste0(tr_filled, local_tr_filled)
+  }
+  
+  table_start <- gsub(
+    "\\?display\\?",
+    display,
+    gsub(
+      "\\?id\\?", 
+      table_id, 
+      strsplit(template, "\\{\\{")[[1]][1]))
+  table_end <- strsplit(strsplit(template, "\\{\\{")[[1]][2], "]]")[[1]][2]
+  
+  table_html <- paste0(
+    table_start,
+    th_filled,
+    "\r\n</tr>\r\n</thead>\r\n<tbody>\r\n",
+    tr_filled,
+    table_end
+  )
+  return(table_html)
+}
